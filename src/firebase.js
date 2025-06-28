2fff7f9d3bf95334aa24b0d822ff68349de5b300
sonusatish34 <sonusatish34@gmail.com>
2025-06-28 08:31:38 +0530

    firebase adjusted

--- src/firebase.js
+++ src/firebase.js
@@ -6,20 +6,14 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDcyVasMykMUUKdh-vB19Wjk2Op5rVOqsE",Add commentMore actions
+  apiKey: "AIzaSyDcyVasMykMUUKdh-vB19Wjk2Op5rVOqsE",
   authDomain: "seoblog-30557.firebaseapp.com",
   projectId: "seoblog-30557",
   storageBucket: "seoblog-30557.firebasestorage.app",
   messagingSenderId: "37436496345",
   appId: "1:37436496345:web:b3916839656826ec7d21c8"
-  // apiKey: "AIzaSyDcyVasMykMUUKdh-vB19Wjk2Op5rVOqsE",
-  // authDomain: "seoblog-30557.firebaseapp.com",
-  // projectId: "seoblog-30557",
-  // storageBucket: "seoblog-30557.firebasestorage.app",
-  // messagingSenderId: "37436496345",
-  // appId: "1:37436496345:web:b3916839656826ec7d21c8"
-
 };
+
 
 const app = initializeApp(firebaseConfig);
 
