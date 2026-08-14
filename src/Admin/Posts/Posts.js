13ead85f70f1bbd3877d74788c8cbb3200956cb3
sonusatish34 <sonusatish34@gmail.com>
2026-08-14 15:02:18 +0530

    mew iss

--- src/Admin/Posts/Posts.js
+++ src/Admin/Posts/Posts.js
@@ -31,7 +31,6 @@ import Loading from "../../layouts/Loading";
 function PostsData({ postsData, currentPage, itemsPerPage, setPostsData }) {
   const [selectedBlogFor, setSelectedBlogFor] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("");
-  console.log(postsData,"postsData");
   
   // Extract unique options for dropdowns dynamically
   const uniqueBlogForOptions = useMemo(
